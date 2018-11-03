a4f37c9fb9bc90449dffe41bceae1d64d104fded
Thomas Lee <thomaslee9474@gmail.com>
2018-11-02 20:10:37 -0700

    users can see reviews

--- app.js
+++ app.js
@@ -3,20 +3,30 @@
 
 // Require npm modules
 var express = require("express");
-var handlebars = require("expres-handlebars")
+var exphbs = require("express-handlebars");
+// var debugger = require("locus");
 // ! Above packages installed.
 
 var app = express()
 
-app.engine("handlebars", handlebars({defaultLayout: "main"}));
-app.set("view engine", handlebars)
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars")
+
+// Mock model
+let reviews = [
+    {title: "Great review", movieTitle: "Batman II"},
+    {title: "Awesome film", movieTitle: "Titanic"}
+]
 
 // Index route
 app.get("/", (req, res) => {
-    res.render("home", {msg: "handlebars is cool"})
+    res.render("reviews-index", {reviews: reviews})
 })
 
+// app.get(".", async (req, res) => {
+//     let reviews = await Review.find()
+// })
 
-app.listen(3000. () => {
+app.listen(3000, () => {
     console.log("App listening to port 3000!");
 })
