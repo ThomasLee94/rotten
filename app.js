0b4c3e4c8cf1180a36e8eab937e4f737d6330fa7
Thomas Lee <thomaslee9474@gmail.com>
2018-11-04 12:20:02 -0800

    Users can see single reviews

--- app.js
+++ app.js
@@ -46,13 +46,23 @@ app.get("/reviews/new", (req, res) => {
 //     let reviews = await Review.find()
 // })
 
-// Create 
+// Create new review
 app.post("/reviews", (req, res) => {
     Review.create(req.body)
         .then((review) => {
             console.log(review);
-            res.redirect("/")
+            res.redirect(`/reviews/${review._id}`)
         }).catch((err) => {
             console.log(err.message);
+        })
+})
+
+
+app.get("/reviews/:id", (req, res) => {
+    Review.findById(req.params.id)
+        .then((review) => {
+            res.render("reviews-show", {review: review})
+        }).catch((err) => {
+            console.log(err.message)
         })
 })
