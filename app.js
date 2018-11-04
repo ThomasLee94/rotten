6c16b5bc98ac167205f1dd7ad070801b1528d7dc
Thomas Lee <thomaslee9474@gmail.com>
2018-11-04 13:04:27 -0800

    users can edit and update reviews

--- app.js
+++ app.js
@@ -4,16 +4,18 @@
 // Require npm modules
 var express = require("express");
 var exphbs = require("express-handlebars");
-var bodyParser = require("body-parser")
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 var mongoose = require("mongoose");
 // var debugger = require("locus");
 // ! Above packages installed.
 
-mongoose.connect('mongodb://localhost/rotten-potatoes');
 var app = express()
+mongoose.connect('mongodb://localhost/rotten-potatoes');
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars")
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(methodOverride("_method"))
 
 app.listen(3000, () => {
     console.log("App listening to port 3000!");
@@ -35,7 +37,6 @@ app.get("/", (req, res) => {
         .catch(err => {
             console.log(err)
         })
-    
 })
 
 // Show new review
@@ -57,7 +58,7 @@ app.post("/reviews", (req, res) => {
         })
 })
 
-
+// Show single review
 app.get("/reviews/:id", (req, res) => {
     Review.findById(req.params.id)
         .then((review) => {
@@ -65,4 +66,14 @@ app.get("/reviews/:id", (req, res) => {
         }).catch((err) => {
             console.log(err.message)
         })
+})
+
+// Edit single review
+app.get("/reviews/:id/edit", (req, res) => {
+    Review.findById(req.params.id, (err, review) => {
+        res.render("reviews-edit", {review: review})
+    })
+    .catch(err => {
+        console.log(err.message)
+    })
 })
